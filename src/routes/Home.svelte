6d0b3927e699c89859694e2f51707558459cbe3b
hayamaster <oksean030330@gmail.com>
2023-07-02 19:58:31 +0000

    달력기능 필요없는 주석 삭제

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,21 +1,11 @@
 <script>
-	// const Calender = () => {
-	// 	console.log(new Date());
-	// 	let nowMonth = new Date().getMonth(); // month는 0부터 시작
-	// };
-	// let nowMonth = new Date();
-	// let firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
-	// let lastDay = `${new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)}`.split(' ');
 	import Calendar from './calendar/Calendar.svelte';
-	let currentDate = new Date();
 </script>
 
 <section class="m-0">
 	<div class="pt-10">
 		<p class="text-3xl text-center">Memories Square</p>
-		<!-- <div>{nowMonth}</div>
-		<div>{lastDay}</div> -->
-		<div><Calendar date={currentDate} /></div>
+		<div><Calendar/></div>
 	</div>
 </section>
 
