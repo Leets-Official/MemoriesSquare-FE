fb3642c20d9d8f9f3de92f433f1df8bbc9f309a2
hayamaster <oksean030330@gmail.com>
2023-07-13 17:36:27 +0000

    auth

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+	<title>Memories Square Auth</title>
+	<meta name="description" content="login" />
+</svelte:head>
+
+<script>
+ import { onMount } from 'svelte';
+
+	let token = '';
+
+	onMount(() => {
+	const url = new URL(window.location.href);
+	const searchParams = new URLSearchParams(url.search);
+	token = searchParams.get('token');
+	console.log(token)
+
+	
+	})
+</script>
