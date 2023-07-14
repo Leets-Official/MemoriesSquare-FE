1e4070409e4a5434c8d94879acbd0346c205fb1a
hayamaster <oksean030330@gmail.com>
2023-07-14 04:27:20 +0000

    feat: 내 앨범 이동 버튼 생성

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -5,7 +5,10 @@
 <section class="m-0">
 	<div class="pt-10">
 		<p class="text-3xl text-center">Memories Square</p>
-		<div><Calendar/></div>
+			<button class="bg-white hover:bg-gray-100 float-right text-gray-800 font-semibold m-6 py-2 px-4 border:none border-gray-400 rounded shadow">
+				내 앨범
+			</button>
+		<div class="clear-right mt-20"><Calendar/></div>
 	</div>
 </section>
 
