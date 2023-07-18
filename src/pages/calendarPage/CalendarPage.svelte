0455e7bfe7fa9359d53a51bbf979cca95208411c
hayamaster <oksean030330@gmail.com>
2023-07-18 15:55:29 +0000

    요일 제거

--- src/pages/calendarPage/CalendarPage.svelte
+++ src/pages/calendarPage/CalendarPage.svelte
@@ -1,20 +1,21 @@
 <script>
-	import Calendar from './calendar/Calendar.svelte';
-	import ImageModal from "./imageupload/ImageModal.svelte";
+  import Calendar from './calendar/Calendar.svelte';
+  import ImageModal from './imageupload/ImageModal.svelte';
 </script>
 
 <section class="m-0">
-	<div class="pt-10">
-		<p class="text-3xl text-center">Memories Square</p>
-		<div class="float-right ">
-			<button class="bg-white hover:bg-gray-100 float-right text-gray-800 font-semibold m-6 py-2 px-4 border:none border-gray-400 rounded shadow">
-				내 앨범
-			</button>
-			
-		</div>
-		<div class="clear-right mt-20">
-			<Calendar/>
-			<ImageModal/>
-		</div>
-	</div>
+  <div>
+    <!-- <p class="text-3xl text-center">Memories Square</p> -->
+    <div class="float-right">
+      <button
+        class="bg-white hover:bg-gray-100 float-right text-gray-800 font-semibold m-6 py-2 px-4 border:none border-gray-400 rounded shadow"
+      >
+        내 앨범
+      </button>
+    </div>
+    <div class="clear-right mt-20">
+      <Calendar />
+      <ImageModal />
+    </div>
+  </div>
 </section>
--- src/pages/calendarPage/calendar/Calendar.svelte
+++ src/pages/calendarPage/calendar/Calendar.svelte
@@ -1,137 +1,147 @@
 <script>
-	import { onMount } from "svelte";
+  import { onMount } from 'svelte';
 
-	let DUMMY_DATA = {
-        2023 : {
-            7 : {
-                1: {
-					"weekDay": "imsy",
-					"photos": 5,
-				},
-				6: {
-					"photos": 10,
-				},
-				7: {
-					"photos": 1,
-				},
-				8: {
-					"photos": 4,
-				},
-				9: {
-					"photos": 1,
-				},
-				10: {
-					"photos": 3,
-				},
-				11: {
-					"photos": 5,
-				},
-				12: {
-					"photos": 7,
-				},
-				13: {
-					"photos": 9,
-				}
-            }
+  let DUMMY_DATA = {
+    2023: {
+      7: {
+        1: {
+          weekDay: 'imsy',
+          photos: 5
+        },
+        6: {
+          photos: 10
+        },
+        7: {
+          photos: 1
+        },
+        8: {
+          photos: 4
+        },
+        9: {
+          photos: 1
+        },
+        10: {
+          photos: 3
+        },
+        11: {
+          photos: 5
+        },
+        12: {
+          photos: 7
+        },
+        13: {
+          photos: 9
         }
+      }
     }
-	const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-	let currentDate = new Date();
-	let thisYear, thisMonth;
-	let days = [];
-	
-	onMount(() => {
-		render();
-	});
+  };
+  const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  let currentDate = new Date();
+  let thisYear, thisMonth;
+  let days = [];
 
-	const render = () => {
-		[thisYear, thisMonth] = [currentDate.getFullYear(), currentDate.getMonth() + 1];
-		const firstDay = new Date(thisYear, thisMonth - 1, 1).getDay();
-		const daysInMonth = new Date(thisYear, thisMonth, 0).getDate();
-		days = Array.from({length: daysInMonth + firstDay}, (_, idx) => {
-			const day = idx + 1 - firstDay;
-			const weekDay = weekDays[idx % 7];
-			return {
-				day: day > 0 ? day : "",
-				weekDay : weekDay
-			};
-		});
+  onMount(() => {
+    render();
+  });
 
-		// 이번 달의 DUMMY_DATA 초기화
-		if(!(thisYear in DUMMY_DATA)){
-			DUMMY_DATA[thisYear] = {};
-		}
-		if (!(thisMonth in DUMMY_DATA[thisYear])){
-			DUMMY_DATA[thisYear][thisMonth] = {};
-		}
-		days.forEach(eachDay => {
-			if(eachDay.day !== "" && !(eachDay.day in DUMMY_DATA[thisYear][thisMonth])){
-				DUMMY_DATA[thisYear][thisMonth][eachDay.day] = {"weekDay": eachDay.weekDay, "photos":0, "bg": "bg-white"};
-			}
-		})
-		// DUMMY_DATA의 background color 초기화
-		for (const day in DUMMY_DATA[thisYear][thisMonth]) {
-			const photoCount = DUMMY_DATA[thisYear][thisMonth][day]["photos"];
-			if (photoCount === 0) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-white";
-			} else if (photoCount === 1) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.1]";
-			} else if (photoCount === 2) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.2]";
-			} else if (photoCount === 3) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.3]";
-			} else if (photoCount === 4) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.4]";
-			}else if (photoCount === 5) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.5]";
-			}else if (photoCount === 6) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.6]";
-			}else if (photoCount === 7) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.7]";
-			}else if (photoCount === 8) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.8]";
-			}else if (photoCount === 9) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.9]";
-			}else if (photoCount >= 10) {
-			DUMMY_DATA[thisYear][thisMonth][day]["bg"] = "bg-emerald-500/[0.5]";
-			}
-		}
+  const render = () => {
+    [thisYear, thisMonth] = [currentDate.getFullYear(), currentDate.getMonth() + 1];
+    const firstDay = new Date(thisYear, thisMonth - 1, 1).getDay();
+    const daysInMonth = new Date(thisYear, thisMonth, 0).getDate();
+    days = Array.from({ length: daysInMonth + firstDay }, (_, idx) => {
+      const day = idx + 1 - firstDay;
+      const weekDay = weekDays[idx % 7];
+      return {
+        day: day > 0 ? day : '',
+        weekDay: weekDay
+      };
+    });
 
-		console.log(DUMMY_DATA)
-	};
+    // 이번 달의 DUMMY_DATA 초기화
+    if (!(thisYear in DUMMY_DATA)) {
+      DUMMY_DATA[thisYear] = {};
+    }
+    if (!(thisMonth in DUMMY_DATA[thisYear])) {
+      DUMMY_DATA[thisYear][thisMonth] = {};
+    }
+    days.forEach((eachDay) => {
+      if (eachDay.day !== '' && !(eachDay.day in DUMMY_DATA[thisYear][thisMonth])) {
+        DUMMY_DATA[thisYear][thisMonth][eachDay.day] = {
+          weekDay: eachDay.weekDay,
+          photos: 0,
+          bg: 'bg-white'
+        };
+      }
+    });
+    // DUMMY_DATA의 background color 초기화
+    for (const day in DUMMY_DATA[thisYear][thisMonth]) {
+      const photoCount = DUMMY_DATA[thisYear][thisMonth][day]['photos'];
+      if (photoCount === 0) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-white';
+      } else if (photoCount === 1) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.1]';
+      } else if (photoCount === 2) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.2]';
+      } else if (photoCount === 3) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.3]';
+      } else if (photoCount === 4) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.4]';
+      } else if (photoCount === 5) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.5]';
+      } else if (photoCount === 6) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.6]';
+      } else if (photoCount === 7) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.7]';
+      } else if (photoCount === 8) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.8]';
+      } else if (photoCount === 9) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.9]';
+      } else if (photoCount >= 10) {
+        DUMMY_DATA[thisYear][thisMonth][day]['bg'] = 'bg-emerald-500/[0.5]';
+      }
+    }
 
-	const previousMonth = () => {
-		currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
-		render();
-	}
+    console.log(DUMMY_DATA);
+  };
 
-	const nextMonth = () => {
-		currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
-		render();
-	};
+  const previousMonth = () => {
+    currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
+    render();
+  };
+
+  const nextMonth = () => {
+    currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+    render();
+  };
 </script>
 
 <div class="calendar">
-	<div class="header flex items-center justify-between mb-8 mt-8"> 
-		<button class="px-4" on:click={previousMonth}>{"<"}</button>
-		<h2 class="text-lg font-semibold">
-			{currentDate.getFullYear()}년 {currentDate.getMonth() + 1}월
-		</h2>
-		<button class="px-4" on:click={nextMonth}>{">"}</button>
-	</div>
+  <div class="header flex items-center justify-between mb-8 mt-8">
+    <button class="px-4" on:click={previousMonth}>{'<'}</button>
+    <h2 class="text-lg font-semibold">
+      {currentDate.getFullYear()}년 {currentDate.getMonth() + 1}월
+    </h2>
+    <button class="px-4" on:click={nextMonth}>{'>'}</button>
+  </div>
 
-	<div class="weekDays grid grid-cols-7">
-		{#each weekDays as weekDay}
-			<div class="bg-gray-200 text-gray-800 border-r border-y border-gray-500 py-2 text-center">{weekDay}</div>
-		{/each}
-	</div>
+  <div class="weekDays grid grid-cols-7">
+    {#each weekDays as weekDay}
+      <div class="bg-gray-200 text-gray-800 border-r border-y border-gray-500 py-2 text-center">
+        {weekDay}
+      </div>
+    {/each}
+  </div>
 
-	<div class="grid grid-cols-7">
-		{#each days as { day, weekDay }}
-			<div class={`${day !== "" ? DUMMY_DATA[thisYear][thisMonth][day]["bg"] : "bg-white"} border-r border-b border-gray-500 p-4 flex flex-col items-center justify-center`}>
-				<div class="text-medium font-semibold">{day}</div>
-				<div class="text-sm">{weekDay}</div>
-			</div>
-		{/each}
-	</div>
+  <div class="grid grid-cols-7">
+    {#each days as { day, weekDay }}
+      <div
+        class={`${
+          day !== '' ? DUMMY_DATA[thisYear][thisMonth][day]['bg'] : 'bg-white'
+        } border-r border-b border-gray-500 p-4 flex flex-col items-center justify-center`}
+      >
+        <div class="text-medium font-medium">{day}</div>
+        <!-- <div class="text-sm">{weekDay}</div> -->
+      </div>
+    {/each}
+  </div>
 </div>
