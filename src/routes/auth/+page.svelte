b5d20416bed5894a0d853451f3ee7bbd7b75c996
hayamaster <oksean030330@gmail.com>
2023-07-18 15:33:08 +0000

    로그인 정보 저장

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   import springFetch from '../../lib/api';
+  import { id, nickname, email, vendor, createdAt, is_login } from '../../lib/store';
 
   onMount(() => {
     const url = new URL(window.location.href);
@@ -8,7 +9,12 @@
     const token = searchParams.get('token');
 
     springFetch('get', '/user/me', '', token, (data) => {
-      console.log(data);
+      $is_login = true;
+      $id = data.result.id;
+      $nickname = data.result.nickname;
+      $email = data.result.email;
+      $vendor = data.result.vendor;
+      $createdAt = data.result.createdAt;
     });
   });
 </script>
